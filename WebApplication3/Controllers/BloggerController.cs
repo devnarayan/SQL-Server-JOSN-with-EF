29a0f595c1cdc5694f970323d076bade0222aa2c
devnarayan.nagar@gmail.com <devnagar@live.com>
2017-09-20 19:45:41 +0530

    Add project files.

---
+++ WebApplication3/Controllers/BloggerController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class BloggerController : Controller
+    {
+        private SchoolContext _context;
+
+        public BloggerController()
+        {
+            _context = new SchoolContext();
+        }
+
+        public ActionResult Index()
+        {
+            var list = _context.Blogs.ToList();
+            var blogs = _context.Blogs.SqlQuery(@"SELECT * FROM Blogs
+                                WHERE JSON_VALUE(Owner, '$.Name') = {0}", "dev")
+                            .ToList();
+            var blogs2 = _context.Blogs
+                          .Where(b => b._Owner.Name == "dev")
+                          .ToList();
+
+
+            return View(_context.Blogs.ToList());
+        }
+
+        public ActionResult Search(string Owner)
+        {
+            // Option 1: .Net side filter using LINQ:
+            var blogs = _context.Blogs
+                            .Where(b => b._Owner.Name == Owner)
+                            .ToList();
+
+            // Option 2: SQL Server filter using T-SQL:
+            //var blogs = _context.Blogs
+            //                .FromSql<Blog>(@"SELECT * FROM Blogs
+            //                    WHERE JSON_VALUE(Owner, '$.Name') = {0}", Owner)
+            //                .ToList();
+
+            return View("Index", blogs);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Blogs.Add(blog);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blog);
+        }
+    }
+}
---
+++ WebApplication3/Models/BloggingContext.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("name=SchoolDBConnectionString")
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Configure domain classes using Fluent API here
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+    public class Blog
+    {
+        [Key]
+        public int BlogId { get; set; }
+
+        [Url]
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+
+        public string Tags { get; set; }
+
+        [NotMapped]
+        public string[] _Tags
+        {
+            get { return Tags == null ? null : JsonConvert.DeserializeObject<string[]>(Tags); }
+            set { Tags = JsonConvert.SerializeObject(value); }
+        }
+
+        public string Owner { get; set; }
+
+        [NotMapped]
+        public Person _Owner
+        {
+            get { return (this.Owner == null) ? null : JsonConvert.DeserializeObject<Person>(this.Owner); }
+            set { Owner = JsonConvert.SerializeObject(value); }
+        }
+    }
+
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [ForeignKey("Blog")]
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+
+    }
+
+    public class Person
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
